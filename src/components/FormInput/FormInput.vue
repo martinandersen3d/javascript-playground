d073d99d6584e3107443a4a94892dc9f3a93329a
martinandersen3d <martinandersen3d@gmail.com>
2020-05-20 00:52:54 +0200

    radio virker

--- src/components/FormInput/FormInput.vue
+++ src/components/FormInput/FormInput.vue
@@ -5,23 +5,31 @@
 // import {eventBus} from '@/main.js'
 // import router from '@/router'
 export default {
+    name:'FormInput',
     // components: {MyComponent},
     // mixins: [MyMixin],
     // use camelCase for names in props, use kebab-case for html-attributes
     props: {
-        errors: [String, Array],
+        errors: [String, Array, Object],
+        description: {
+            type: String
+        },
         label: {
-          type: String
+            type: String
         },
         name: {
-          type: String
+            type: String
         },
+        options: [String, Array, Object, Boolean],
         type: {
             // type: String,
           validator: function (value) {
             // The value must match one of these strings
             return ['button','checkbox','color','date','datetime-local','email','file','hidden','image','month','number','password','radio','range','reset','search', 'select','submit','tel','text','time','url','week'].indexOf(value) !== -1
           }
+        },
+        value: {
+            type:  [String, Number, Boolean]
         },
 
     },
@@ -48,37 +56,114 @@ export default {
             }
         },
         errorsComputed(){
-            if( typeof this.errors === 'string' && this.errors === ''){
-               return [this.errors]
+            // if string
+            if( typeof this.errors === 'string' && this.errors !== ''){
+                let arr = []
+                arr.push(this.errors)
+               return arr;
             }
-            return this.errors;
+            // If Array
+            if(Array.isArray(this.errors)){
+                return this.errors;
+            }
+            // If object
+            if(this.errors instanceof Object){
+                if( this.name !==undefined || this.id !== undefined ){
+                   let val = this.name || this.id
+                   return this.errors[val]
+                }
+            }
+        },
+        optionsComputed(){
+            // if string
+            if( typeof this.options === 'string' && this.options === ''){
+                let obj = {}
+                obj[this.options] = this.options
+               return obj
+            }
+            // If Array
+            if(Array.isArray(this.options)){
+                let obj = {}
+                for( let item of this.options){
+                    obj[item] = item
+                }
+                return this.obj;
+            }
+            // If object
+            if(this.options instanceof Object){
+                   return this.options
+            }
+        },
+        radioGS: {
+            get: function() {
+                this.value
+            },
+            set: function(newValue) {
+                this.value = newValue
+            }
         }
-    },
 
+    },
     methods: {
-        todo() {
+        isType(val){
+            let type = this.type.toLowerCase() || ''
+            let defauls = ['button','checkbox','color','date','datetime-local','email','file','hidden','image','month','number','password','range','reset','search','submit','tel','text','time','url','week']
+            // 'default'
+            if( val === 'default' && defauls.includes(type) ){
+               return true
+            }
+
+            // if(  val === 'checkbox' && type === 'checkbox' ){ return true}
+            if(  val === 'radio' && type === 'radio' ){ return true}
+            if(  val === 'select' && type === 'select' ){ return true}
+                
+            
 
         }
     }, //__________________________________________
 
-    // data() {
-    //     return {
-    //         _label: labelComputed,
-    //     };
-    //},
+    data() {
+        return {
+            radioValue: 3,
+        };
+    },
 }
 </script>
 
 <template><div class='FormInput '>
-    <label v-if="labelComputed" :for="idComputed" class="fi-label">{{ labelComputed }} 🛈</label>
-    <input v-if="type" :id="idComputed" :type="type" value="sfd" class="fi-input">
-    <div class="fi-div-errors" v-if="errorsComputed">
+    <label v-if="labelComputed" :for="idComputed" class="fi-label">{{ labelComputed }}</label>
+    
+    <!-- Default types -->
+    <input v-if="isType('default')" :id="idComputed" :type="type" value="sfd" class="fi-input">
+
+    <!-- Radio type -->
+    <div v-if="isType('radio') && options">
+        <div   class="fi-input"  v-for="( item_value, itemName, item_index) in optionsComputed" :key="item_index">
+              <input type="radio" :name="name" :id="itemName" v-model="radioGS" :value="item_value"  >
+              <label  :for="itemName">{{ itemName }}</label>
+        </div>
+    </div>
+
+    <!-- Select type -->
+    <div v-if="isType('select') && options">
+        <div >
+            <select  class="fi-input" v-model="value">
+                <option v-for="( value, name, index) in optionsComputed" value="value" :key="index" >{{ name }}</option>
+            </select>
+        </div>
+    </div>
+
+
+    <!-- Description -->
+     <div class="fi-div-errors" v-if="errorsComputed" :class="!errorsComputed ? 'fi-div-errors-hide' : ''">
         <ul v-for="( errorItem, index) in errorsComputed" :key="index" class="fi-ul-errors">
             <li>{{ errorItem }}</li>
         </ul>
-    </div>
+    </div> 
+
+    <!-- Description -->
     <div class="fi-div-description" v-if="!errorsComputed">
-        fsdffsd
+        {{ description }}
     </div>
 
 </div>
@@ -90,7 +175,7 @@ label.fi-label{
     width: 100%;
     text-transform: capitalize;
 }
-input.fi-input{
+.fi-input{
     width: 100%;
     display: block;
 }
@@ -102,6 +187,17 @@ div.fi-div-errors{
     background-color: #f8d7da;
     border-color: #f5c6cb;
     border-radius: 5px;
+    font-size:80%;
+    max-height:650px;
+    transition:all 200ms ease;
+}
+div.fi-div-errors-hide{
+  max-height:0px;
+  overflow:hidden;
+  color: rgba(255, 0, 0, 0);
+  padding:0px;
+  padding-left: 8px;
+  background-color:transparent;
 }
 ul.fi-ul-errors{
     width: 100%;
@@ -118,4 +214,9 @@ ul.fi-ul-errors > li{
     font-size:80%;
     margin: 8px 0 8px 0;
 }
+
+div.fi-checkbox-group{
+    display: block;
+}
+
 </style>
--- src/components/FormInput/FormInputPage.vue
+++ src/components/FormInput/FormInputPage.vue
@@ -30,7 +30,12 @@ export default {
 </script>
 
 <template><div class='FormInputPage '>
-    <FormInput type="button" name="My_name" label="my_label" :errors="0"></FormInput>
+    <FormInput type="button" name="My_name0" label="my_label0" :errors="'whatever'"></FormInput>
+    <FormInput type="button" name="My_name" label="my_label1" :errors="['error1','error2','error3']"></FormInput>
+    <FormInput type="button" name="My_name" label="my_label2" :errors="{'My_name':['a', 'b'], 'other':['c']}"></FormInput>
+    <FormInput type="text" name="My_name" label="my_label3"></FormInput>
+    <FormInput type="select" name="My_select" label="my_select" :options="{'value1':true, 'value2':false, 'value3':1}"></FormInput>
+    <FormInput type="radio" name="My_radio" label="my_radio" :options="{'value1':true, 'value2':false, 'value3':5}"></FormInput>
 </div>
 </template>
 
